8559006005254bf620cb1eea35ca1741137d0fa3
Jeroen <jeroenooms@gmail.com>
2014-07-29 17:22:08 +0200

    update example page

--- app.js
+++ app.js
@@ -18,6 +18,16 @@ oh.user.whoami().done(function(username){
 	//make sure we don't timeout
 	oh.keepalive();
 
+	//This is where you put your app code
+	//Silly example app shows user info
 
+	oh.user.info().done(function(x){
+		$.each(x[username].campaigns, function(urn, name){
+			$("#campaigntable tbody").append("<tr><td>" + urn + "</td><td>" + name + "</td></tr>")
+		});
+		$.each(x[username].classes, function(urn, name){
+			$("#classtable tbody").append("<tr><td>" + urn + "</td><td>" + name + "</td></tr>")
+		});		
+	})
 
 });
